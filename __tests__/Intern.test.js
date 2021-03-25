faf2d83b48c0a05fe0e08e33aa0944ce6610a0b8
Tess Austin <tessaustin92@gmail.com>
2021-03-25 13:29:12 -0600

    intern + failed testing

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+test('intern object', () => {
+    const intern = new Intern('Dave');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('intern school', () => {
+    const intern = new Intern('Dave');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('role of employee', () => {
+    const intern = new Intern('Dave');
+
+    expect(intern.getRole()).toEqual('Intern');
+})
