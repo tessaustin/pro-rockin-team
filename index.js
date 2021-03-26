6f3b4a1844795f0d36b632edf4ef1d062973e696
Tess Austin <tessaustin92@gmail.com>
2021-03-25 18:48:43 -0600

    q no longer appear, trying to figure out why it would error after engineer or intern q

--- index.js
+++ index.js
@@ -7,179 +7,193 @@ const Manager = require('./lib/Manager');
 //Packages needed for app
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateHTML = require('./src/generateHTML')
+const generateHTML = require('./src/generateHTML');
+const Prompt = require('inquirer/lib/prompts/base');
+const teamData = [];
 
 //Array of questions for employee input 
-const questions = [
-    {
-        //Start of Managers questions
-        type: 'input',
-        name: 'name',
-        message: 'Manager of this team? (Required)',
-        //make sure user entered input
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter the manager's name!");
-                return false;
+const qManager = () => {
+    return inquirer
+        .prompt([
+            {
+                //Start of Managers questions
+                type: 'input',
+                name: 'name',
+                message: 'Manager of this team? (Required)',
+                //make sure user entered input
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the manager's name!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Manager's ID: (Required)",
+                //make sure user entered input
+                validate: nameInput => {
+                    if (isNaN(nameInput)) {
+                        console.log("Please enter the manager's ID!")
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Manager's email: (Required)",
+                //make sure user entered input
+                validate: email => {
+                    if (email) {
+                        return true;
+                    } else {
+                        console.log('Please enter an email!')
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "Manager's office number: (Required)",
+                //make sure user entered input
+                validate: nameInput => {
+                    if (isNaN(nameInput)) {
+                        console.log('Please enter an office number!')
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "Manager's ID: (Required)",
-        //make sure user entered input
-        validate: nameInput => {
-            if (isNaN(nameInput)) {
-                console.log("Please enter the manager's ID!")
-                return false;
-            } else {
-                return true;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "Manager's email: (Required)",
-        //make sure user entered input
-        validate: email => {
-            if (email) {
-                return true;
-            } else {
-                console.log('Please enter an email!')
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: "Manager's office number: (Required)",
-        //make sure user entered input
-        validate: nameInput => {
-            if (isNaN(nameInput)) {
-                console.log('Please enter an office number!')
-                return false;
-            } else {
-                return true;
-            }
-        }
-    },
-
-    //Adding another employee to team profile
-    {
-        type: 'confirm',
-        name: 'confirmAddEmployee',
-        message: 'Would you like to add more team members?',
-        default: false
-    },
-    {
-        type: 'list',
-        name: 'role',
-        message: "Employee's role",
-        choices: ['Engineer', 'Intern']
-    },
-    {
-        //employee name
-        type: 'input',
-        name: 'name',
-        message: "Employee's name? (Required)",
-        //make sure user entered input
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your name!');
-                return false;
-            }
-        }
-    },
-    {
-        //employee ID
-        type: 'input',
-        name: 'id',
-        message: "Employee's ID: (Required)",
-        //make sure user entered input
-        validate: nameInput => {
-            if (isNaN(nameInput)) {
-                console.log("Please enter the employee's ID!")
-                return false;
-            } else {
-                return true;
-            }
-        }
-    },
-    {
-        //employee email
-        type: 'input',
-        name: 'email',
-        message: "Employee's email: (Required)",
-        //make sure user entered input
-        validate: email => {
-            if (email) {
-                return true;
-            } else {
-                console.log('Please enter an email!')
-                return false;
-            }
-        }
-    },
-    {
-        //employee Github Username
-        type: 'input',
-        name: 'github',
-        message: "Employee's GitHub Username: (Required)",
-        when: (input) => input.role === 'Engineer',
-        //make sure user entered input
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Must enter your GitHub username!');
-                return false;
-            }
-        }
-    },
-    {
-        //employee's school'
-        type: 'input',
-        name: 'school',
-        message: "Intern's school: (Required)",
-        when: (input) => input.role === "Intern",
-        //make sure user entered input
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter the intern's school!")
-            }
-        }
-    }
-
-
-];
-
-//Function to generate HTML file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Your rockin team profile has been created! Check it out!')
-    });
-}
-
-// function to initialize program
-function init() {
-    inquirer.prompt(questions)
-        .then(function (userInput) {
-            console.log(userInput)
-            writeToFile("HTML", generateHTML(userInput));
-        });
+        ])
 };
 
-// Function call to initialize app
-init()
+
+//Adding another employee to team profile
+const qEmployee = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'confirmAddEmployee',
+                message: 'Would you like to add more team members?',
+                default: false
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "Employee's role",
+                choices: ['Engineer', 'Intern']
+            },
+            {
+                //employee name
+                type: 'input',
+                name: 'name',
+                message: "Employee's name? (Required)",
+                //make sure user entered input
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                //employee ID
+                type: 'input',
+                name: 'id',
+                message: "Employee's ID: (Required)",
+                //make sure user entered input
+                validate: nameInput => {
+                    if (isNaN(nameInput)) {
+                        console.log("Please enter the employee's ID!")
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                //employee email
+                type: 'input',
+                name: 'email',
+                message: "Employee's email: (Required)",
+                //make sure user entered input
+                validate: email => {
+                    if (email) {
+                        return true;
+                    } else {
+                        console.log('Please enter an email!')
+                        return false;
+                    }
+                }
+            },
+            {
+                //employee Github Username
+                type: 'input',
+                name: 'github',
+                message: "Employee's GitHub Username: (Required)",
+                when: (input) => input.role === 'Engineer',
+                //make sure user entered input
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log('Must enter your GitHub username!');
+                        return false;
+                    }
+                }
+            },
+            {
+                //employee's school'
+                type: 'input',
+                name: 'school',
+                message: "Intern's school: (Required)",
+                when: (input) => input.role === "Intern",
+                //make sure user entered input
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's school!")
+                    }
+                }
+            }
+        ])
+};
+
+
+//Function to generate HTML file
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        //error 
+        if (err) {
+            console.log(err);
+            return;
+            //team profile created 
+        }
+        console.log('Your rockin team profile has been created! Check it out!')
+    })
+};
+
+// function to initialize program
+qManager()
+    .then(qEmployee)
+    .then(teamData => {
+        return generatePage(teamData)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
+    .catch(err => {
+        console.log(err);
+    });
